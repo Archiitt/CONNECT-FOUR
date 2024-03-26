bf23d4cc410b73b478366435de2a89e755a178ad
ARCHIT <145273087+Archiitt@users.noreply.github.com>
2024-03-27 00:05:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ var player2Color = 'rgb(237, 45, 73)';
 
 var game_on = true;
 var table = $('table tr');
+// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
+function reportWin(rowNum, colNum) {
+	console.log("You won starting at this row,col");
+	console.log(rowNum);
+	console.log(colNum);
+}
+// Change the color of a button
+function changeColor(rowIndex, colIndex, color) {
+	return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color', color);
+}
+
+// Report Back to current color of a button
+function returnColor(rowIndex, colIndex) {
+	return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+// Take in column index, returns the bottom row that is still gray
+function checkBottom(colIndex) {
+	var colorReport = returnColor(5, colIndex);
+	for (var row = 5; row > -1; row--) {
+		colorReport = returnColor(row, colIndex);
+		if (colorReport === 'rgb(128, 128, 128)') {
+			return row
+		}
+	}
+}
